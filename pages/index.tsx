fc03120d7b7a62f284093104566d1ad875adb264
GYNFLO <gynflo@gmail.com>
2023-05-26 11:50:13 +0200

    Init Tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-900 h-screen w-screen flex items-center">
+      <div className="w-full text-center">
+        <button className="bg-white p-2">Login with Google</button>
+      </div>
+    </div>
+  )
+}
